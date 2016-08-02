2074144b659cc02eb54a5661ebc7b030bf7c6d84
Kenneth Yu <kenyu@ucdavis.edu>
2016-08-01 19:09:33 -0700

    add json_spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -99,4 +99,8 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+  # Adding JsonSpec helpers
+  # https://github.com/collectiveidea/json_spec
+  config.include JsonSpec::Helpers
 end
